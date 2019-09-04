55a2da4b575cf3a6c1b5e28e48e6b33293800797
Consultant <consultant@Consultants-MacBook-Pro.local>
2019-09-04 16:49:19 -0400

    first commit

---
+++ Testing with GitHUb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing with GitHUb
+//
+//  Created by Consultant on 9/4/19.
+//  Copyright © 2019 Mobile Apps Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
